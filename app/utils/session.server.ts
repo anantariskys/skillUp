d6375d5ca2b875c9c55756ec93aea20a31f0875d
anantariskys <gamersindo555@gmail.com>
2024-11-09 09:32:21 +0700

    Create session.server.ts

---
+++ app/utils/session.server.ts
@@ -0,0 +1,23 @@
+// utils/session.server.ts
+import { createCookieSessionStorage } from "@remix-run/node";
+
+const sessionStorage = createCookieSessionStorage({
+  cookie: {
+    name: "__session",
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    secrets: [process.env.SESSION_SECRET || "secret"],
+    sameSite: "lax",
+  },
+});
+
+export async function getSession(cookieHeader: string | null) {
+  return sessionStorage.getSession(cookieHeader);
+}
+
+export async function commitSession(session: any) {
+  return sessionStorage.commitSession(session);
+}
+export async function destroySession(session: any) {
+  return sessionStorage.destroySession(session);
+}
